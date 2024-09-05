1ad7f86f8447e1a31191a5bfbc9ad03d3ff7bbbd
Madihaafzal043 <145421849+Madihaafzal043@users.noreply.github.com>
2024-09-05 09:39:54 +0500

    Add files via upload

---
+++ Java script/var,let,const.js
@@ -0,0 +1,6 @@
+var fullName = "Madiha";
+let Id = 1233;
+const address = "fetahpur";
+var Email = "madiha.gc@gmailcom";
+console.table([fullName,Id,address,Email]); 
+ address ="layyah";  // Error genrate
